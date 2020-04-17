3d1b0f83012aac306e183e5671936b25e60558c5
VincentDong <149212768@qq.com>
2020-04-17 18:06:29 +0800

    新增socket数据请求支持

--- src/socket.js
+++ src/socket.js
@@ -1,25 +1,41 @@
-if ('WebSocket' in window) {
-  alert('您的浏览器支持 WebSocket!');
+import _ from 'lodash';
+class socket {
+  constructor(server, port, account, password) {
+    this.account = account;
+    this.password = password;
+    this.server = server;
+    this.port = port;
+  }
+  static load(callback) {
+    if ('WebSocket' in window) {
+      // 打开一个 web socket
+      var uri = 'ws://' + this.server + ':' + this.port;
+      var ws = new WebSocket(uri);
+      // 连接建立后的回调函数
+      ws.onopen = function () {
+        // Web Socket 已连接上，使用 send() 方法发送数据
+        ws.send(this.account + ':' + this.password);
+      };
 
-  // 打开一个 web socket
-  var ws = new WebSocket('ws://localhost:9998/echo');
+      // 接收到服务器消息后的回调函数
+      ws.onmessage = function (evt) {
+        var data = evt.data;
+        if (_.isFunction(callback)) {
+          console.log(data);
+          callback(data);
+        }
+      };
 
-  ws.onopen = function () {
-    // Web Socket 已连接上，使用 send() 方法发送数据
-    ws.send('发送数据');
-    alert('数据发送中...');
-  };
-
-  ws.onmessage = function (evt) {
-    var received_msg = evt.data;
-    alert('数据已接收...');
-  };
-
-  ws.onclose = function () {
-    // 关闭 websocket
-    alert('连接已关闭...');
-  };
-} else {
-  // 浏览器不支持 WebSocket
-  alert('您的浏览器不支持 WebSocket!');
+      // 连接关闭后的回调函数
+      ws.onclose = function () {
+        // 关闭 websocket
+        console.log('连接已关闭...');
+      };
+    } else {
+      // 浏览器不支持 WebSocket
+      console.log('您的浏览器不支持 WebSocket!');
+    }
+  }
 }
+
+export default socket;
